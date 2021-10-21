41f3ba7d87bed7c447e98f3d3e8065a3b817a57a
Грант Кесиян <www.grantix1@mail.ru>
2021-10-22 00:01:56 +0300

    админка, правила для аутентификации и авторизации пользователей

--- CompanyServices/CompanyServices/Startup.cs
+++ CompanyServices/CompanyServices/Startup.cs
@@ -1,19 +1,15 @@
-using CompanyServices.Domain;
-using CompanyServices.Domain.Repositories.Abstract;
-using CompanyServices.Domain.Repositories.EntityFramework;
-using CompanyServices.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CompanyServices.Domain;
+using CompanyServices.Domain.Repositories.Abstract;
+using CompanyServices.Domain.Repositories.EntityFramework;
+using CompanyServices.Service;
 
 namespace CompanyServices
 {
@@ -32,6 +28,7 @@ namespace CompanyServices
             services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
             services.AddTransient<DataManager>();
 
+            //подключаем контекст Ѕƒ
             services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
 
             //настраиваем identity систему
@@ -55,9 +52,19 @@ namespace CompanyServices
                 options.SlidingExpiration = true;
             });
 
-            services.AddControllersWithViews()
-                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
-                .AddSessionStateTempDataProvider();
+            //настраиваем политику авторизации дл€ Admin area
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("AdminArea", policy => { policy.RequireRole("admin"); });
+            });
+
+            //добавл€ем сервисы дл€ контроллеров и представлений (MVC)
+            services.AddControllersWithViews(x =>
+                {
+                    x.Conventions.Add(new AdminAreaAuthorization("Admin", "AdminArea"));
+                })
+                //выставл€ем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -79,6 +86,7 @@ namespace CompanyServices
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("admin", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
